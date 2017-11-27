0bb2e9c22317b8e7dae2183007370da56fb9fc37
AHowardC <achsvgusaeng@yahoo.com>
2017-11-26 23:12:38 -0500

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+	var reg = /[\W_]/g;
+	var smallStr = str.toLowerCase().replace(reg, '');
+
+	var reversed = smallStr.split('').reverse().join('');
+	if (reversed === str) {
+		return true	
+
+	}
+	return false
+}
+
+palindrome('racecar')
+// .split('')
+// .reverse()
+// .join('')
