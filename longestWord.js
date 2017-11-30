4466f98a7739aa71e8d60351f834bf0f96fadfb2
AHowardC <achsvgusaeng@yahoo.com>
2017-11-29 22:36:36 -0500

    fix function

--- longestWord.js
+++ longestWord.js
@@ -1,16 +1,17 @@
 function findLongestWord(str){
-  var words = str.split(' ');
-  
-  
+  return str.split(' ').sort(function(a,b){
+    return b.length - a.length
+  })[0]
+ 
 }
 
 findLongestWord('The quick brown fox jumped over the lazy dog');
-
-var fruits = [
-  'apple',
-  'mango',
-  'cherries'
-];
-for (var i=0; i<fruits.length; i++){
-  console.log(fruits[i]);
-}
+//  var words = str.split(' ');
+// var fruits = [
+//   'apple',
+//   'mango',
+//   'cherries'
+// ];
+// for (var i=0; i<fruits.length; i++){
+//   console.log(fruits[i]);
+// }
