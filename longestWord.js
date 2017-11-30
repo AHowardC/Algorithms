a2680ca88f2d1b1f040e732285980ffea561c609
AHowardC <achsvgusaeng@yahoo.com>
2017-11-29 22:23:59 -0500

    starting

---
+++ longestWord.js
@@ -0,0 +1,16 @@
+function findLongestWord(str){
+  var words = str.split(' ');
+  
+  
+}
+
+findLongestWord('The quick brown fox jumped over the lazy dog');
+
+var fruits = [
+  'apple',
+  'mango',
+  'cherries'
+];
+for (var i=0; i<fruits.length; i++){
+  console.log(fruits[i])
+}
