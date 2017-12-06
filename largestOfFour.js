b11afe9766d400eecf09348e83a0e20021ff6e52
AHowardC <achsvgusaeng@yahoo.com>
2017-12-06 00:01:30 -0500

    use nested for loop

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,7 +1,7 @@
 function largestOfFour(arr) {
 	var max = [];
 	for (var i = 0; i < arr.length; i++) {
-
+		using nested loop
 		for (var j = 0; j < arr[i].length; j++) {
 			console.log(arr[i][j]);
 		}
