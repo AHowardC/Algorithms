277b7e980f8e47d9a0d25f6d52626400fd5a6b81
AHowardC <achsvgusaeng@yahoo.com>
2017-12-06 00:21:46 -0500

    update

--- largestOfFour.js
+++ largestOfFour.js
@@ -1,6 +1,9 @@
 function largestOfFour(arr) {
 	var max = [];
 	for (var i = 0; i < arr.length; i++) {
+		console.log(arr[i])
+
+		
 		using nested loop
 		for (var j = 0; j < arr[i].length; j++) {
 			console.log(arr[i][j]);
