fea2a6ea35e0cc668ef748628adacfd3e32922be
AHowardC <achsvgusaeng@yahoo.com>
2017-12-07 00:12:01 -0500

    confTheEnding

---
+++ confirmTheEnding.js
@@ -0,0 +1,10 @@
+function confirmTheEnding(str, target) {
+	return str.substr(-target.length) === target;
+	// if(str.endsWith(target)){
+	// 	return true
+	// }
+	// return false
+	
+	// return str.endsWith(target)
+	
+}
