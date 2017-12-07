f8f6300d63d2714f2990c05a22bef8a2d91fa613
AHowardC <achsvgusaeng@yahoo.com>
2017-12-07 12:07:52 -0500

    multiAns

--- confirmTheEnding.js
+++ confirmTheEnding.js
@@ -1,10 +1,12 @@
 function confirmTheEnding(str, target) {
+	// using .substr(takes 2 params)
 	return str.substr(-target.length) === target;
 	// if(str.endsWith(target)){
 	// 	return true
 	// }
 	// return false
 	
+	// using .endsWith() method
 	// return str.endsWith(target)
 	
 }
