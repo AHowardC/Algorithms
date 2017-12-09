c56ceb109eca93b9fa63816dbcccb4ab5dd2173b
AHowardC <achsvgusaeng@yahoo.com>
2017-12-08 23:57:08 -0500

    working on rsnt

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -1,6 +1,12 @@
 function repeatStringNumTimes(str, num) {
   // repeat after me
-
+  if (num < 0) {
+    return "";
+  }
+  return str.repeat(num);
 }
 
 repeatStringNumTimes('abc', 3)
+
+
+console.log('hello'.repeat(3));
