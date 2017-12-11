766febff8475895dfeb01976212cb26cf9659abe
AHowardC <achsvgusaeng@yahoo.com>
2017-12-10 22:35:19 -0500

    dif way

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -1,6 +1,13 @@
 function repeatStringNumTimes(str, num) {
   // #8 repeat after me
-
+  var final = ''
+  if(num < 0) {
+    return "";
+    for (var i = 0; i < num; i++){
+      final = final + str;
+    }
+  }
+return final;
 }
 
 repeatStringNumTimes('abc', 3)
