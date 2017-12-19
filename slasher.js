d2132edede2757941549a865bb15665e1e8194ae
AHowardC <achsvgusaeng@yahoo.com>
2017-12-19 09:14:00 -0500

    nc

---
+++ slasher.js
@@ -0,0 +1,9 @@
+function Slasher(arr, amt) {
+
+  return arr;
+}
+
+Slasher([1,2,3,4,5,6,7], 2)
+
+// .splice() method changes the original array.
+// .slice() method does not change the original array.
