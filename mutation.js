b69a9dc5980e19f4ad1ffb790f1513ecf29ccc60
AHowardC <achsvgusaeng@yahoo.com>
2017-12-21 00:59:15 -0500

    ud

--- mutation.js
+++ mutation.js
@@ -2,6 +2,10 @@ function mutation(arr) {
   var firstWord = [0].toLowerCase();
   var secondWord = [1].toLowerCase();
 
+  for (var i = 0; i < secondWord.length; i++) {
+    
+  }
+
 
   return arr;
 }
