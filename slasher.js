75c9bdc8330149d98b7e159cbfaefb2a2cce87fc
AHowardC <achsvgusaeng@yahoo.com>
2017-12-20 23:01:12 -0500

    nn

--- slasher.js
+++ slasher.js
@@ -5,5 +5,6 @@ function Slasher(arr, rm) {
 
 Slasher([1,2,3,4,5,6,7], 2)
 
-// .splice() method changes the original array.
+// .splice() method changes the original array. the first arg is where to start from
+// and the second arg is where to stop.
 // .slice() method does not change the original array.
