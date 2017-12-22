d23a862d23b6f496b2ddffa7434720c65a487243
AHowardC <achsvgusaeng@yahoo.com>
2017-12-22 11:34:53 -0500

    addUp

---
+++ addUp.js
@@ -0,0 +1,17 @@
+function addUp(){
+  var numbers = [1,2,3,4,5,6,7];
+  var total = 0;
+  for(var index = 0; index < numbers.length; index++){
+    total += numbers[index];
+
+  }
+
+return total;
+}
+addUp();
+
+// 1. Define your function with a disctriptive name.
+// 2. set your variables needed for the function.
+// 3. set up a for/while loop to increment your array.
+// 4. return the result you want to get back.
+// 5. invoke/call your function by the name of the function.
