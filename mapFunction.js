7f92b9f1a6ac45a6d896ebc6451c69e843bdfbf6
AHowardC <achsvgusaeng@yahoo.com>
2017-12-23 00:28:06 -0500

    join,repeat

--- mapFunction.js
+++ mapFunction.js
@@ -1,2 +1,13 @@
 // the map function makes a copy of an original array
 // and manipulates the copied version.
+var array = [1,2,3,4,5];
+var newArray = array.map(function(index){
+  return index * 2;
+})
+
+var array = [1,2,3,4,5];
+var newArray = array.map(function(index){
+  return index * 2;
+});
+console.log(newArray);
+console.log(newArray.join('').repeat(2));
