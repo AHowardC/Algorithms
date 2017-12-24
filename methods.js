a213af36945a1b2833f986ec72a1e6a8b4a34851
AHowardC <achsvgusaeng@yahoo.com>
2017-12-23 20:47:47 -0500

    mm

--- methods.js
+++ methods.js
@@ -1,10 +1,12 @@
+.forEach() iterates over an array BUT also changes the original array.
 .join() converts an array into a string
 .split() converts a string into an array
 .splice()
 .slice()
+.reduce()
 .repeat()takes one arg; a number of times to repeat a string. only works on strings.
+.reverse()
 .toUpperCase() converts a string of letters from LowerCase letters to toUpperCase letters.
 .toLowerCase() converts a string of letters from upperCase letters to LowerCase letters.
 .map(function(index to iterate))makes a copy of an array and does not change the original array.
 it iterates over an array.
-.forEach() iterates over an array BUT also changes the original array.
