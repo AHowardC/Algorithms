b0a97d1113739091721cc0c4f875e61b1073322b
AHowardC <achsvgusaeng@yahoo.com>
2017-12-25 19:02:18 -0500

    newn

--- methods.js
+++ methods.js
@@ -1,4 +1,4 @@
-.charAt()
+.charAt() returns a string of the index spicified. if you dont pass it an index it will default to the 0th index
 .find()
 .forEach() iterates over an array BUT also changes the original array.
 .join() converts an array into a string
@@ -10,7 +10,7 @@
 .splice()The splice() method changes the contents of an array by removing existing elements and/or adding new elements.
 .slice()The slice() method returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included).
  The original array will not be modified.
-
+.sort()
 .toString() converts and object into a string.
 .toUpperCase() converts a string of letters from LowerCase letters to toUpperCase letters.
 .toLowerCase() converts a string of letters from upperCase letters to LowerCase letters.
