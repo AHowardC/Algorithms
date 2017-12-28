9d617fbb189139d7f2c396714c476b46e41078fb
AHowardC <achsvgusaeng@yahoo.com>
2017-12-28 00:09:39 -0500

    nn

--- methods.js
+++ methods.js
@@ -9,7 +9,7 @@ each element in the array (from left to right) to reduce it to a single value.
 .repeat()takes one arg; a number of times to repeat a string. only works on strings.
 .replace()The replace() method returns a new string with some or all matches of a pattern replaced by a replacement.
 The pattern can be a string or a RegExp.The replacement can be a string or a function to be called for each match.
-.reverse()
+.reverse() reverses an array
 .split() converts a string into an array
 .splice()The splice() method changes the contents of an array by removing existing elements and/or adding new elements.
 .slice()The slice() method returns a shallow copy of a portion of an array into a new array object selected from begin to end (end not included).
