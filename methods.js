23a018c4887c484626039a1feadb656d48184524
AHowardC <achsvgusaeng@yahoo.com>
2017-12-30 20:35:50 -0500

    nn

--- methods.js
+++ methods.js
@@ -5,7 +5,7 @@
 .join() converts an array into a string
 .reduce(function(a,b){a+b})The reduce() method applies a function against an accumulator and
 each element in the array (from left to right) to reduce it to a single value.
-.concat()
+.concat() combines two strings together.
 .repeat()takes one arg; a number of times to repeat a string. only works on strings.
 .replace()The replace() method returns a new string with some or all matches of a pattern replaced by a replacement.
 The pattern can be a string or a RegExp.The replacement can be a string or a function to be called for each match.
