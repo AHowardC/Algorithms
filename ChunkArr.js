1abdb297c4be1aef35eb7c9bb75eee976cbd5b15
AHowardC <achsvgusaeng@yahoo.com>
2017-12-31 20:32:46 -0500

    review

--- ChunkArr.js
+++ ChunkArr.js
@@ -9,7 +9,7 @@ function ChunkArrayInGroup(arr, size) {
 
 ChunkArrayInGroup([1,2,3,4,5,6,7,8,9], 3);
 
-// ChunkArrayInGroup takes two args; first an array
+// ChunkArrayInGroup takes two args; first an array, second a number
 // methods .push(), .slice()
 // .splice() method will change original array
 // .reduce() reduces the array to a single value.
