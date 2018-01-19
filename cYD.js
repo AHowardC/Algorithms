522b78445360c0f7f12a620a685618e45965a0aa
AHowardC <achsvgusaeng@yahoo.com>
2018-01-18 23:22:51 -0500

    ncc

---
+++ cYD.js
@@ -0,0 +1,9 @@
+function canYouDrink(arr){
+  var canDrink = [];
+  for (var i = 0; i < age.length; i++) {
+    if (age[i] >= 21) {
+      canDrink.push(age[i]);
+    }
+  }
+  console.log(canDrink);
+}
