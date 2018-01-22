cc492300705d5ae47761887d829dcfbfada7b3ec
AHowardC <achsvgusaeng@yahoo.com>
2018-01-21 21:17:54 -0500

    cmf

--- destroy.js
+++ destroy.js
@@ -3,14 +3,20 @@
 function destroy(arr) {
   // Remove all the values
   // should filter out elements from arr that match optional args
-  console.log(arr);
-  return arr;
+  var args = Array.from(arguments);
+  args.splice(0,1);
+  var targets = args;
+
+  // console.log(arr);
+  return arr.filter(function(num){
+    return targets.indexOf(num) === -1;
+  });
 }
 
 // filter out the optional args
 
+// array.from(takes an object)
 // .filter(function(){}) a higher order function
-.indexOf()method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+// .indexOf()method returns the first index at which a given element can be found in the array, or -1 if it is not present.
 // .includes() returns boolean if found or not
 // arguments keyword gives everything back in object format
-// array.from(takes an object)
