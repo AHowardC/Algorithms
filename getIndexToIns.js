fd6813ad42a6c30796562210d822ba4b12ea89ed
AHowardC <achsvgusaeng@yahoo.com>
2018-01-23 21:46:02 -0500

    log

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -1,9 +1,10 @@
 function getIndexToIns(arr, num) {
   // find place in sorted array
+  console.log(arr);
   arr.sort(function (a, b) {
     return a - b;
   })
-  
+
 }
 
 getIndexToIns([40,50], 45)
