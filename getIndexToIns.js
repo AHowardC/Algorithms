0d3964113d8824b38d88f739551883a066ec15c4
AHowardC <achsvgusaeng@yahoo.com>
2018-01-23 21:52:57 -0500

    hrt

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -4,7 +4,16 @@ function getIndexToIns(arr, num) {
   arr.sort(function (a, b) {
     return a - b;
   })
+
   console.log(arr);
+
+  for (var i = 0; i < arr.length; i++) {
+    if (num <= arr[i]) {
+      return i;
+    }
+  }
+
+  return arr.length;
 }
 
-getIndexToIns([40,50], 45)
+getIndexToIns([40,50,3], 45)
