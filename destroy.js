dbcd937bcb3622a2cc7839d2490c082aa39c7541
AHowardC <achsvgusaeng@yahoo.com>
2018-01-25 14:17:34 -0500

    mvr

--- destroy.js
+++ destroy.js
@@ -10,12 +10,12 @@ function destroy(arr) {
   console.log(arr);
   return arr.filter(function(num){
     return targets.indexOf(num) === -1;
-    
+
   });
 }
 
-// filter out the optional args
 
+// filter out the optional args
 // array.from(takes an object)
 // .filter(function(){}) a higher order function
 // .indexOf()method returns the first index at which a given element can be found in the array, or -1 if it is not present.
