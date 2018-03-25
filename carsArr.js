58e15cfbb9f4d729d7f395e0e4dbc1c3a96dc95b
AHowardC <achsvgusaeng@yahoo.com>
2018-03-25 17:26:03 -0400

    arr of obj

---
+++ carsArr.js
@@ -0,0 +1,28 @@
+// var cars = [
+//     {make:'bmw', model:'i550', color:'white'},
+//     {make:'toyota', model:'camry', color:'red'},
+//     {make:'mazda', model:'versa', color:'black'},
+//     {make:'volvo', model:'s60', color:'silver'},
+//     {make:'toyota', model:'prius', color:'green'},
+//     {make:'nissan', model:'altima', color:'silver'},
+//     {make:'ford', model:'escape', color:'black'},
+//     {make:'honda', model:'civic', color:'grey'},
+//     {make:'toyota', model:'corolla', color:'grey'},
+//     {make:'volkswagon', model:'golf', color:'white'},
+//     {make:'nissan', model:'versa', color:'black'},
+//     {make:'bmw', model:'i335', color:'black'},
+//     {make:'hyundai', model:'sonata', color:'black'},
+//     {make:'mercedes-benz', model:'versa', color:'black'},
+//     {make:'honda', model:'accord', color:'blue'},
+//     {make:'chevy', model:'cruz', color:'yellow'},
+//     {make:'ford', model:'focus', color:'silver'},
+//   ];
+  
+// var brands = cars.filter((car)=>car.make ==='volvo');
+
+// console.log(brands);
+
+// ''+ ''
+
+name = "aname";
+console.log(name);
