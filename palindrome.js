83ca761ecaae45a0ba7b8817945fcac22c65f0c5
Akil <achsvgusaeng@yahoo.com>
2018-09-18 19:53:10 +0000

    two

--- palindrome.js
+++ palindrome.js
@@ -16,3 +16,24 @@ palindrome('racecar')
 // .split('')
 // .reverse()
 // .join('')
+
+// if reversed str is the same as forward str then it is a palindrome
+// return a boolean
+
+function palindrome(str){
+  let reversed = '';
+  
+
+  for(let i of str){
+    reversed = i + reversed;
+    console.log(reversed);
+  }
+  // const reversed = str.split('').reverse().join('');
+  if(str == reversed){
+    return true;
+  }
+  return false;
+}
+
+
+console.log(palindrome('tat'));
