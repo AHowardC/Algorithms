b844c236bd5bbc1ac3f4cadce6bb7d352595829a
Akil <achsvgusaeng@yahoo.com>
2018-09-19 19:44:26 +0000

    notes

--- maxChar.js
+++ maxChar.js
@@ -22,3 +22,28 @@ function maxChar(str){
 }
 
 console.log(maxChar('Hello There!'));
+
+function max(str){
+//     find the count of each character in a string
+//     3 variables needed :max counter :maxChar :charObj
+    const charMap = {};
+    let max =0;
+    let maxChar ='';
+    
+    //     loop through the string and put characters into obj
+    for(let i of str){
+      charMap[i] = charMap[i] +1 || 1;
+    }
+    
+    //     loop through the object to find largest character count
+    for(let i in charMap){
+      //     if object prop is > than max update the max
+      if(charMap[i]> max){
+        max= charMap[i];
+        maxChar =i;
+      }
+    }
+    return maxChar;
+}
+
+console.log(max('hello'));
