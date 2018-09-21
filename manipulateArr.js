d8ba073cbe34ac417cc1f2daf0e4c0a9c2f0c63e
Akil <achsvgusaeng@yahoo.com>
2018-09-21 14:38:24 +0000

    manipulateArr

---
+++ manipulateArr.js
@@ -0,0 +1,23 @@
+
+function manipulateArr(arr){
+  const even = [];
+    for(let i=arr.length - 1; i >=0; i--){
+//       console.log(arr[i])
+      for(let j =0; j <=arr[i].length -1; j++){
+        if(arr[i][j] % 2 == 0){
+          even.push(arr[i][j]);
+        }
+          console.log(arr[i][j]);
+        console.log(even);
+      }
+      
+    }
+}
+manipulateArr([
+        [1,
+        2],
+        [3,
+        4],
+        [5,
+        6]
+        ]);
