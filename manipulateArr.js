93fbcfb395458306bc14ef8ba72ccad5a1af9252
Akil <achsvgusaeng@yahoo.com>
2018-09-23 01:20:21 +0000

    new looping with map

--- manipulateArr.js
+++ manipulateArr.js
@@ -21,3 +21,17 @@ manipulateArr([
         [5,
         6]
         ]);
+
+function nestedLoop(arr){
+//   return the items inside the nested arr in reverse
+    var results = [];
+    var array = arr.map(value=> value.map(value=> results.push(value)))
+    var reversed =results.reverse().map(value=> console.log(value))
+   
+}
+
+nestedLoop([
+  ['one',2],
+  [3,'four'],
+  ['five', 6]
+]);
