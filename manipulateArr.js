4e8ccf0f69ab4bb83e70219c901e03b6a1d375ec
Akil <achsvgusaeng@yahoo.com>
2018-09-23 01:24:39 +0000

    diff note

--- manipulateArr.js
+++ manipulateArr.js
@@ -22,16 +22,3 @@ manipulateArr([
         6]
         ]);
 
-function nestedLoop(arr){
-//   return the items inside the nested arr in reverse order
-    var results = [];
-    var array = arr.map(value=> value.map(value=> results.push(value)))
-    var reversed =results.reverse().map(value=> console.log(value))
-   
-}
-
-nestedLoop([
-  ['one',2],
-  [3,'four'],
-  ['five', 6]
-]);
