59f6982d4f63348f4fe20f791b9ab36278600b0c
Akil <achsvgusaeng@yahoo.com>
2018-09-25 15:13:40 +0000

    cleanup

--- ChunkArr.js
+++ ChunkArr.js
@@ -12,5 +12,5 @@ ChunkArrayInGroup([1,2,3,4,5,6,7,8,9], 3);
 // ChunkArrayInGroup takes two args; first an array, second a number
 // methods .push(), .slice()
 // .splice() method will change original array
-// .reduce(higerOrderFunc) reduces the array to a single value.
-()
+// .reduce() reduces the array to a single value.
+
