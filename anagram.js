18d050495cc18b45059cdae809ca7af514df2063
Akil <achsvgusaeng@yahoo.com>
2018-09-27 20:49:10 +0000

    uses helper

---
+++ anagram.js
@@ -0,0 +1,17 @@
+
+// checks if string A is equal to string B
+function anagrams(strA, strB){
+  return cleanStr(strA) === cleanStr(strB);
+}
+
+// removes white space, keeps only letters and removes special characters, converts to lowercase and sorts characters
+function cleanStr(str){
+  return str.replace(/[^\w]/g, '')
+  .toLowerCase()
+  .split('')
+  .sort()
+  .join('');
+}
+
+
+console.log(anagrams('hello world','world! HELLO'));
